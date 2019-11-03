52897f1522f788fcda79f2d52a7a0fab9ab9fd71
DeVeL_AnOnIk <27363518+anonik9900@users.noreply.github.com>
2019-11-03 20:27:34 +0000

    Add files via upload
                                              ____                         
     ,--,     ,--,     ,---,                ,'  , `.    ,---,   .--.--.    
     |'. \   / .`|    '  .' \            ,-+-,.' _ | ,`--.' |  /  /    '.  
     ; \ `\ /' / ;   /  ;    '.       ,-+-. ;   , || |   :  : |  :  /`. /  
     `. \  /  / .'  :  :       \     ,--.'|'   |  ;| :   |  ' ;  |  |--`   
      \  \/  / ./   :  |   /\   \   |   |  ,', |  ': |   :  | |  :  ;_     
       \  \.'  /    |  :  ' ;.   :  |   | /  | |  || '   '  ;  \  \    `.  
        \  ;  ;     |  |  ;/  \   \ '   | :  | :  |, |   |  |   `----.   \ 
       / \  \  \    '  :  | \  \ ,' ;   . |  ; |--'  '   :  ;   __ \  \  | 
      ;  /\  \  \   |  |  '  '--'   |   : |  | ,     |   |  '  /  /`--'  / 
    ./__;  \  ;  \  |  :  :         |   : '  |/      '   :  | '--'.     /  
    |   : / \  \  ; |  | ,'         ;   | |`-'       ;   |.'    `--'---'   
    ;   |/   \  ' | `--''           |   ;/           '---'                 
    `---'     `--`                  '---'                                  
                                                                           
    
    
    ***********************************V1.0*****************************************
    
    
    
    [Functions -]
    
    *Install Spotify
    *Update PKG
    *Install Figlet
    *Install InstaInsane
    *Instal Custom Shell
    *Auto-Setup My Kali dist
    
    
    [Devolved By:  -Anonik]

---
+++ XAMIS/XAMIS.sh
@@ -0,0 +1,139 @@
+
+#!/bin/bash
+
+ echo "__   __             __  __   _____    _____ "
+ echo "\ \ / /     /\     |  \/  | |_   _|  / ____|"
+ echo " \ V /     /  \    | \  / |   | |   | (___  "
+ echo "  > <     / /\ \   | |\/| |   | |    \___ \ "
+ echo " / . \   / ____ \  | |  | |  _| |_   ____) |"
+ echo "/_/ \_\ /_/    \_\ |_|  |_| |_____| |_____/ "
+                                             
+echo""
+echo""
+
+
+
+echo "---- UPDATE PKG ------"
+apt-get update
+echo "---- DONE UPDATE PKG ----"
+
+echo "---- Installing Spotify ----"
+echo""
+ 
+ echo "  _____                   _     _    __         "
+ echo " / ____|                 | |   (_)  / _|        "
+ echo "| (___    _ __     ___   | |_   _  | |_   _   _ "
+ echo " \___ \  | '_ \   / _ \  | __| | | |  _| | | | |"
+ echo " ____) | | |_) | | (_) | | |_  | | | |   | |_| |"
+ echo "|_____/  | .__/   \___/   \__| |_| |_|    \__, |"
+ echo "         | |                               __/ |"
+ echo "         |_|                              |___/ "
+
+echo""
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4773BD5E130D1D45
+
+echo ""
+echo""
+
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+echo""
+echo ""
+
+echo "[UpDaTiNg Spotify Resources]. . . ."
+sudo apt update
+echo ""
+echo""
+echo "[Installing Spotify. . .]"
+echo "["
+sudo apt install spotify-client
+echo "]"
+
+echo""
+
+echo "Spotify was Successfully Installed"
+echo""
+echo""
+
+echo "  ____  _     ____  _____  ____  _        ____  _     _____ _     _    "
+echo " /   _\/ \ /\/ ___\/__ __\/  _ \/ \__/|  / ___\/ \ /|/  __// \   / \   "
+echo " |  /  | | |||    \  / \  | / \|| |\/||  |    \| |_|||  \  | |   | |   "
+echo " |  \__| \_/|\___ |  | |  | \_/|| |  ||  \___ || | |||  /_ | |_/\| |_/\'"
+echo "'\____/\____/\____/  \_/  \____/\_/  \|  \____/\_/ \|\____\'\____/\____/"
+
+echo""
+echo""
+
+	sudo apt-get install figlet
+	git clone https://github.com/busyloop/lolcat
+	sudo gem install lolcat
+	cd lolcat/bin && gem install lolcat
+	cd ../../..
+	cd -
+
+	echo""
+	echo"[MANUAL - -]"
+	echo "Dopo Aver aperto gedit copiare e incollare dopo l'ultimo *fi* le seguenti stringe"
+	echo""
+	echo"clear"
+	echo "figlet -f slant 'Anonik Hack' |lolcat"
+	gedit /etc/bash.bashrc
+
+	echo""
+	echo""
+
+
+
+
+   echo "  ____                   __             ____                                      "
+   echo "'/  _/   ____    _____  / /_  ____ _   /  _/   ____    _____  ____ _   ____   ___ "
+   echo "/ /    / __ \  / ___/ / __/ / __ `      /    / __    / ___/ / __ `/  / __ \ / _ \ "
+echo " _/ /    / / / / (__  ) / /_  / /_/ /  _/ /    / / / / (__  ) / /_/ /  / / / // '__/"
+echo "/___/   /_/ /_/ /____/  \__/  \__,_/  /___/   /_/ /_/ /____/''\__''' '/_/ /_/''\___/"
+
+
+echo ""
+
+echo""
+
+echo "[GIT CLONE ]"
+echo ""
+cd ../../..
+git clone https://github.com/thelinuxchoice/instainsane
+echo ""
+cd instainsane
+echo ""
+chmod +x install.sh
+chmod +x instainsane.sh
+echo ""
+
+echo "[INSTALLING TOR]"
+echo""
+echo apt-get install tor
+echo""
+echo "[ALL DONE. ENJOY KALI ANONIK]"
+
+
+                                                                                    
+
+
+                                                                        
+
+                                                                     
+                                                                      
+
+
+
+                                                                      
+
+
+
+
+
+
+
+                                                   
+                                                   
+
+
+
